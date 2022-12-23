d025826061b1a5fac1885d4132c2c4ebc645ec20
blackflag6613 <blackflag6613@gmail.com>
2022-12-23 12:42:47 +0300

    form 4.8

---
+++ store/store/users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from users.models import User
+from users.forms import UserLoginForm
+
+def login(request):
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    return render(request, 'users/registration.html')
