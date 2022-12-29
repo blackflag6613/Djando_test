7dbda3447b9ee67eb318d7880f8576c3f9b25481
blackflag6613 <blackflag6613@gmail.com>
2022-12-29 19:58:30 +0300

    4.11_registrations

--- store/store/users/urls.py
+++ store/store/users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from users.views import login,registration
-
+from users.views import login, registration
 
 app_name = 'users'
 urlpatterns = [
-    path('login/',login, name='login'),
-    path('regisration/',registration, name='registration')
+    path('login/', login, name='login'),
+    path('regisration/', registration, name='registration'),
 ]
--- store/store/users/views.py
+++ store/store/users/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
-from users.models import User
-from users.forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
+
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
