f62a173e39026ed40b93d5f5c9053598202f5370
blackflag6613 <blackflag6613@gmail.com>
2023-01-31 11:37:10 +0300

    finish modul 5

--- store/store/users/views.py
+++ store/store/users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse
-
+from products.models import Basket
+from django.contrib.auth.decorators import login_required
 
 def login(request):
     if request.method == 'POST':
@@ -32,7 +33,7 @@ def registration(request):
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -43,7 +44,14 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+
+
+    context = {
+        'title': 'Store - Профиль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'users/profile.html', context)
 
 
